fb071135c8eda10b0f932d7471998daaf9e4e2b1
Sylvia Wang <sylviamiaowang@gmail.com>
2024-05-29 02:17:03 +1000

    dropdown menu added

--- scripts.js
+++ scripts.js
@@ -1,3 +1,12 @@
 document.getElementById("myButton").addEventListener("click", function() {
     alert("Button clicked!");
 });
+
+function toggleMenu() {
+    var menu = document.getElementById("dropdownMenu");
+    if (menu.style.display === "flex") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "flex";
+    }
+}
