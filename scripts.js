e3c8757f28732946a570f9236fe2e36235e87f0b
Sylvia Wang <sylviamiaowang@gmail.com>
2024-05-29 02:17:37 +1000

    removed test button

--- scripts.js
+++ scripts.js
@@ -1,7 +1,3 @@
-document.getElementById("myButton").addEventListener("click", function() {
-    alert("Button clicked!");
-});
-
 function toggleMenu() {
     var menu = document.getElementById("dropdownMenu");
     if (menu.style.display === "flex") {
