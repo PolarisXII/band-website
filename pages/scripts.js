cbcad458c461a50e4b7157aea97f6e632a6ece88
Sylvia Wang <sylviamiaowang@gmail.com>
2024-05-30 03:32:03 +1000

    toggle menu routing

--- pages/scripts.js
+++ pages/scripts.js
@@ -8,3 +8,17 @@ function toggleMenu() {
         menu.style.transform = "translateX(0)";
     }
 }
+
+document.querySelectorAll(".menu-item").forEach(item => {
+    item.addEventListener('click', function() {
+        let pathName = item.textContent.trim();
+
+        if (pathName === "Home") {
+            window.location.href = "/";
+        } else {
+            window.location.href = `/${(pathName)}`;
+        }
+    });
+});
+
+
--- server.js
+++ server.js
@@ -10,6 +10,22 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'pages/index.html'));
 });
 
+app.get('/band', (req, res) => {
+    res.sendFile(path.join(__dirname, 'pages/band.html'));
+});
+
+app.get('/videos', (req, res) => {
+    res.sendFile(path.join(__dirname, 'pages/videos.html'));
+});
+
+app.get('/photos', (req, res) => {
+    res.sendFile(path.join(__dirname, 'pages/photos.html'));
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, 'pages/contact.html'));
+});
+
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
 });
